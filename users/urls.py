2996bcfbd3e96d9fa7ff9ee419f2c424d5f3ff10
anesbetch <156690535+anesbetch@users.noreply.github.com>
2025-04-14 22:38:01 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home_view, name='home'),
+    path('', views.department_list, name='department-list'),
+    path('new/', views.department_create, name='department-create'),
 ]
