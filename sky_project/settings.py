86e046595591cb7cd94f20ed34e9342ba3c9f683
melikovsheroz <sherozmelikov043@gmail.com>
2025-04-20 23:27:23 +0100

    Deleted teams app !

--- sky_project/settings.py
+++ sky_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sky_project',
-    'users.apps.UsersConfig',
-    'teams.apps.TeamsConfig',
+    'users.apps.UsersConfig'
+    
     
 ]
 
--- teams/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import DepartmentLeader,Department,Team
-
-admin.site.register(DepartmentLeader)
-admin.site.register(Department)
-admin.site.register(Team)
