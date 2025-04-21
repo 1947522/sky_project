2879673b3c958801eef166f29d2a402b52c3a2dd
anesbetch <156690535+anesbetch@users.noreply.github.com>
2025-04-21 18:10:48 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
-    #path('home/', views.home_view, name='home'),
+    path('home/', views.home_view, name='home'),
     path('admin_page/', views.admin_page, name='admin_page'),
     path('home/', views.home_view, name='home'),
     path('departmentleader/', views.departmentleader_view, name='departmentleader'),
+    path('departments/', views.department_list, name='department_list'),
+    path('departments/new/', views.department_create, name='department_create'),
 ]
