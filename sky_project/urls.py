b071263449a031e58d792df9a12930998d2e065b
melikovsheroz <sherozmelikov043@gmail.com>
2025-04-21 19:57:19 +0100

    .gitignore is being added

--- sky_project/urls.py
+++ sky_project/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),  # Include our accounts app's URLs
     # path('departments/', include('departments.urls')),
-   
+    path('departmentleader/', include('users.urls')),
 ]
